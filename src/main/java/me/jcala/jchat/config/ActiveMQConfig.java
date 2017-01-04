d57edf01ae44f0b4118fb712cc588cfabba357ea
jcala <jcalaz@163.com>
2017-01-05 02:03:30 +0800

    加入了人嵌入式ActiveMQ的配置，并且可以正确运行

---
+++ src/main/java/me/jcala/jchat/config/ActiveMQConfig.java
@@ -0,0 +1,49 @@
+package me.jcala.jchat.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+import java.util.Collections;
+
+@Configuration
+@EnableJms
+public class ActiveMQConfig {
+    private static final String DEFAULT_BROKER_URL = "tcp://localhost:61616";
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    // TODO: 17-01-05  加入jmooc项目时要修改信任的包名
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(DEFAULT_BROKER_URL);
+        connectionFactory.setTrustedPackages(Collections.singletonList("me.jcala.jchat"));
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        // This provides all boot's default to this factory, including the message converter
+        configurer.configure(factory, connectionFactory);
+        // You could still override some of Boot's default if necessary.
+        return factory;
+    }
+
+}
