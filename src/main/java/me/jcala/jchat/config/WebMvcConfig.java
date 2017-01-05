40e2f928f9757407a21bfaf24d69b6049591d990
jcala <jcalaz@163.com>
2017-01-05 21:46:23 +0800

    登录拦截器排除登录路径，使登录页面可以正常运行

--- src/main/java/me/jcala/jchat/config/WebMvcConfig.java
+++ src/main/java/me/jcala/jchat/config/WebMvcConfig.java
@@ -3,7 +3,10 @@ package me.jcala.jchat.config;
 import me.jcala.jchat.interceptor.UserSecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -18,6 +21,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(securityInterceptor).addPathPatterns("/**");//配置登录拦截器拦截路径
+        registry.addInterceptor(securityInterceptor).addPathPatterns("/**").excludePathPatterns("/login");//配置登录拦截器拦截路径
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseSuffixPatternMatch(false);
     }
 }
